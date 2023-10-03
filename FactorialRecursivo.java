a3a0cf032bacac3c23073605b70aa1cb9ebafe25
lawitadelimon <lau72abrvjua@gmail.com>
2023-10-02 21:45:08 -0600

    02 Factorial Recursivo

---
+++ FactorialRecursivo.java
@@ -0,0 +1,20 @@
+public class FactorialRecursivo {
+
+    private static int factorial(int num){
+        if (num == 0) {
+            return 1;
+        } else{
+            return num * factorial(num - 1);
+        }
+    }
+
+
+    public static void main(String[]args) {
+        int num = 5;
+        int resultado;
+
+        System.out.println("Programa que calcula el factorial de un numero");
+        resultado = factorial(num);
+        System.out.println(" El factorial de " + num + " es: " + resultado);
+    }
+}
